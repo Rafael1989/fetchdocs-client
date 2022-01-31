c46b82bcb91d0a0f065c52b00cc299b721764257
unknown <flns_rafa@hotmail.com>
2022-01-31 20:10:55 +0100

    first commit

--- src/main/java/com/mkyong/java11/jep321/FetchDocsApp.java
+++ src/main/java/com/mkyong/java11/jep321/FetchDocsApp.java
@@ -26,6 +26,14 @@ public class FetchDocsApp {
 
         System.out.println(result);
 
+        result = createCustomer();
+
+        System.out.println(result);
+
+        result = createCustomerSupplier();
+
+        System.out.println(result);
+
     }
 
     private static String createEnvironment() throws InterruptedException, ExecutionException, TimeoutException {
@@ -38,6 +46,45 @@ public class FetchDocsApp {
         HttpRequest request = HttpRequest.newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
                 .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/createEnvironment"))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    private static String createCustomer() throws InterruptedException, ExecutionException, TimeoutException {
+        JSONObject json = new JSONObject();
+        json.put("api_key","ASWZ-3U19-5N0P-YKZR-10BN-R2IO-90NH-SNWY");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/createCustomer"))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    private static String createCustomerSupplier() throws InterruptedException, ExecutionException, TimeoutException {
+        JSONObject json = new JSONObject();
+        json.put("customer_prim_uid",163464);
+        json.put("supplier_prim_uid",4774);
+        json.put("active","true");
+        json.put("fields","{\"USERNAME\": \"163464\", \"PASSWORD\": \"1i1c42y3\"}");
+        json.put("iv","1i1c42y3");
+        json.put("api_key","ASWZ-3U19-5N0P-YKZR-10BN-R2IO-90NH-SNWY");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/createCustomerSupplier"))
                 .setHeader("Content-Type", "application/json")
                 .build();
 
