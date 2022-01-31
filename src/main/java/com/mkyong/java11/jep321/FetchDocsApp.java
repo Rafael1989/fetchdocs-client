a4e4cbc79fd11a6eede410dcdd6f12743a2c8fcb
unknown <flns_rafa@hotmail.com>
2022-01-31 21:46:33 +0100

    first commit

--- src/main/java/com/mkyong/java11/jep321/FetchDocsApp.java
+++ src/main/java/com/mkyong/java11/jep321/FetchDocsApp.java
@@ -34,6 +34,18 @@ public class FetchDocsApp {
 
         System.out.println(result);
 
+        result = testQueue();
+
+        System.out.println(result);
+
+        result = getDocumentsQueue();
+
+        System.out.println(result);
+
+        result = getDocument();
+
+        System.out.println(result);
+
     }
 
     private static String createEnvironment() throws InterruptedException, ExecutionException, TimeoutException {
@@ -85,6 +97,60 @@ public class FetchDocsApp {
         HttpRequest request = HttpRequest.newBuilder()
                 .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
                 .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/createCustomerSupplier"))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    private static String testQueue() throws InterruptedException, ExecutionException, TimeoutException {
+        JSONObject json = new JSONObject();
+        json.put("prim_uid",163475);
+        json.put("api_key","ASWZ-3U19-5N0P-YKZR-10BN-R2IO-90NH-SNWY");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/testQueue"))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    private static String getDocumentsQueue() throws InterruptedException, ExecutionException, TimeoutException {
+        JSONObject json = new JSONObject();
+        json.put("api_key","ASWZ-3U19-5N0P-YKZR-10BN-R2IO-90NH-SNWY");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/getDocumentsQueue"))
+                .setHeader("Content-Type", "application/json")
+                .build();
+
+        CompletableFuture<HttpResponse<String>> response =
+                httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+
+        String result = response.thenApply(HttpResponse::body).get(5, TimeUnit.SECONDS);
+        return result;
+    }
+
+    private static String getDocument() throws InterruptedException, ExecutionException, TimeoutException {
+        JSONObject json = new JSONObject();
+        json.put("prim_uid","163497");
+        json.put("customer_secret","1i1c42y3");
+        json.put("api_key","ASWZ-3U19-5N0P-YKZR-10BN-R2IO-90NH-SNWY");
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                .uri(URI.create("https://api.stage.fetchdocs.io/fetch/v1/getDocument"))
                 .setHeader("Content-Type", "application/json")
                 .build();
 
